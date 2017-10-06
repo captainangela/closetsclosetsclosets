597399ef6a21f741bfb1e1923b427fa697f216bc
angela lui <angela.pl.lui@gmail.com>
2017-10-06 16:36:17 -0700

    test

--- model.py
+++ model.py
@@ -12,6 +12,16 @@ import urlparse
 
 db = SQLAlchemy()
 
+urlparse.uses_netloc.append("postgres")
+url = urlparse.urlparse(os.environ["DATABASE_URL"])
+
+conn = psycopg2.connect(
+    database=url.path[1:],
+    user=url.username,
+    password=url.password,
+    host=url.hostname,
+    port=url.port
+)
 
 ################################ORM########################################
 
